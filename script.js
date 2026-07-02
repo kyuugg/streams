d752fc4a02fe2c3652be4a13af0a456e373e7cc1
ayukyuu <arya72480@gmail.com>
2026-07-03 00:33:35 +0200

    added all javascript (dont ask)

---
+++ script.js
@@ -0,0 +1,24 @@
+const nav = document.querySelector("#buttons-nav");
+const input = document.querySelector("#sidebar-search");
+const streamFrame = document.querySelector("#stream-frame");
+
+function generateSource(streamer) {
+    return `https://player.twitch.tv/?channel=${streamer}&parent=127.0.0.1`;
+}
+
+input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        const newButton = document.createElement("button");
+        newButton.className = "sidebar-item";
+        newButton.textContent = input.value;
+        nav.append(newButton);
+        input.value = "";
+    }
+});
+
+nav.addEventListener("click", (event) => {
+    if (event.target.matches(".sidebar-item")) {
+        const streamer = event.target.textContent;
+        streamFrame.src = generateSource(streamer);
+    }
+});
