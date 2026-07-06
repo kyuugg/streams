1c618951920252c2f9e6508436b70fa464607de7
ayukyuu <arya72480@gmail.com>
2026-07-06 13:27:50 +0200

    add comments to javascript

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// constants
 const nav = document.querySelector("#buttons-nav");
 const input = document.querySelector("#sidebar-input");
 const streamFrame = document.querySelector("#stream-frame");
 const heading = document.querySelector("#stream-name");
 
+// utility functions
 function generateSource(streamer) {
     return `https://player.twitch.tv/?channel=${streamer}&parent=streams.kyuu.gg`;
 }
 
+// input enter behaviour
 input.addEventListener("keydown", (event) => {
     if (event.key === "Enter" && input.value.trim()) {
         const buttonDiv = document.createElement("div");
@@ -29,12 +32,17 @@ input.addEventListener("keydown", (event) => {
     }
 });
 
+// button click behaviour
 nav.addEventListener("click", (event) => {
+    // if streamer button is clicked
     if (event.target.matches(".sidebar-streamer-button")) {
         const streamer = event.target.textContent;
         heading.textContent = event.target.textContent;
         streamFrame.src = generateSource(streamer);
+
+        // if delete button is clicked
     } else if (event.target.matches(".sidebar-delete-button")) {
+        // check if currently selected streamer is the one thats being deleted
         if (event.target.className.includes(heading.textContent)) {
             streamFrame.src = "";
             heading.textContent = "";
